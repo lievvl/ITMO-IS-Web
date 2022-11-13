02b4c84f94239d3fee0cc6922a5769119590e08b
lievvl <37303364+lievvl@users.noreply.github.com>
2022-11-13 20:40:20 +0300

    feat: done TodoList

---
+++ LabDir/TodoScript.js
@@ -0,0 +1,30 @@
+var globalList = {};
+
+(function () {
+    console.log(localStorage.hasOwnProperty("TodoList"))
+    if (localStorage.hasOwnProperty("TodoList"))
+    {
+        globalList = JSON.parse(localStorage.getItem("TodoList"));
+        window.onload = function () {
+            for (let key in globalList) {
+                addItemToDOM(globalList[key]);
+            }
+        }
+    }
+})();
+
+function addItem() {
+    let input = document.getElementById("todo_input");
+    addItemToDOM(input.value);
+    let key = Object.keys(globalList).length + 1;
+    globalList[key] = input.value;
+    console.log(globalList);
+    localStorage.setItem("TodoList", JSON.stringify(globalList));
+}
+
+function addItemToDOM(value) {
+    let list = document.getElementById("todo_list");
+    let tag = document.createElement("li");
+    tag.textContent = value;
+    list.append(tag);
+}
