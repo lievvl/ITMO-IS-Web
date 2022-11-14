0c6f53bde3a3d47943fc2c3781fa38b8e25423e7
lievvl <37303364+lievvl@users.noreply.github.com>
2022-11-14 16:50:34 +0300

    feat: added

--- LabDir/SpeedScript.js
+++ LabDir/SpeedScript.js
@@ -1,7 +1,7 @@
 (function () {
     let startDate = Date.now();
-    window.onload = function() {
-        let loadTime = Date.now() - startDate;
+    window.addEventListener("load", function () {
+        let loadTime = window.performance.timing.loadEventStart - window.performance.timing.navigationStart;
         let footer = document.getElementById('footer_container');
         let p = document.createElement('P');
         p.innerText = "Load Time: " + (loadTime / 1000).toString() + "s";
@@ -9,11 +9,28 @@
 
         let path = document.location.pathname.split('/');
         let name = path[path.length - 1].split('.')[0];
+        let ulChilds = document.getElementById("hamburger_menu_id").children;
+        let ar = []
+        for (i = 0; i < ulChilds.length; i++) {
+            if (i === 0) {
+                continue;
+            }
+            let a = ulChilds[i].children[0];
+            let filename = ulChilds[i].children[0].attributes[1].nodeValue;
+            filename = filename.split('/');
+            filename = filename[filename.length - 1].split('.')[0]
+            ar.push({
+                filename: a
+            });
+        }
+        console.log(ar);
+        console.log(document.getElementById("hamburger_menu_id").children[2].children[0].attributes[1].nodeValue);
         if (name == "index") {
             document.getElementById("hamburger_menu_id").children[1].children[0].classList.add("hamburger-menu__onPage");
         }
         if (name == "table") {
-                document.getElementById("hamburger_menu_id").children[2].children[0].classList.add("hamburger-menu__onPage");
+            document.getElementById("hamburger_menu_id").children[2].children[0].classList.add("hamburger-menu__onPage");
         }
-    }
+
+    })
 })();
