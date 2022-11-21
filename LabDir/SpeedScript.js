759b744df74b2e09c21bd168c2571892bf5d02d9
lievvl <37303364+lievvl@users.noreply.github.com>
2022-11-21 20:02:35 +0300

    fix: made fixes

--- LabDir/SpeedScript.js
+++ LabDir/SpeedScript.js
@@ -1,7 +1,6 @@
 (function () {
-    let startDate = Date.now();
     window.addEventListener("load", function () {
-        let loadTime = window.performance.timing.loadEventStart - window.performance.timing.navigationStart;
+        let loadTime = (window.performance.timing.loadEventStart - window.performance.timing.navigationStart);
         let footer = document.getElementById('footer_container');
         let p = document.createElement('P');
         p.innerText = "Load Time: " + (loadTime / 1000).toString() + "s";
@@ -20,17 +19,18 @@
             filename = filename.split('/');
             filename = filename[filename.length - 1].split('.')[0]
             ar.push({
-                filename: a
+                filename: filename,
+                a: a
             });
         }
         console.log(ar);
         console.log(document.getElementById("hamburger_menu_id").children[2].children[0].attributes[1].nodeValue);
-        if (name == "index") {
-            document.getElementById("hamburger_menu_id").children[1].children[0].classList.add("hamburger-menu__onPage");
+        for (i = 0; i < ar.length; i++)
+        {
+            if (ar[i].filename == name) {
+                ar[i].a.classList.add("hamburger-menu__onPage");
+                break;
+            }
         }
-        if (name == "table") {
-            document.getElementById("hamburger_menu_id").children[2].children[0].classList.add("hamburger-menu__onPage");
-        }
-
     })
 })();
